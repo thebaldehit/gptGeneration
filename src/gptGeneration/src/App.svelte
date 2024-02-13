a933f63a4047eee978070c02ae9026dd15fcbf93
thebladehit <boqdanyarmolka@gmail.com>
2024-02-14 00:02:09 +0200

    Added Token verification logic

--- src/gptGeneration/src/App.svelte
+++ src/gptGeneration/src/App.svelte
@@ -1,9 +1,35 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Token from "./components/Token.svelte";
+
+  let token = localStorage.getItem('OpenAIToken');
+
+  const verifyToken = async (apiKey) => {
+    const request = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content: 'it is a test'
+          }
+        ]
+      })
+    });
+    if (request.ok) {
+      localStorage.setItem('OpenAIToken', apiKey);
+      token = apiKey;
+    }
+  };
 </script>
 
 <div class="container">
-  <h1>Text</h1>
+  {#if token}
+  {:else}
+    <Token buttonHandler={verifyToken}/>
+  {/if}
 </div>
---
+++ src/gptGeneration/src/components/Token.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let buttonHandler;
+  let apiKey;
+</script>
+
+<div class="content">
+  <input type="text" placeholder="API KEY" bind:value={apiKey}>
+  <button on:click={() => {buttonHandler(apiKey)}}>GO</button>
+</div>
+
+<style>
+  input {
+    width: 500px;
+    height: 50px;
+    margin-right: 20px;
+    border: none;
+    outline: none;
+    box-sizing: border-box;
+    transition: .1s linear;
+    padding: 1% 2%;
+    border-radius: 5px;
+  }
+
+  input:focus {
+    border: 2px solid #646cff;
+    outline: none;
+  }
+
+  button {
+    width: 150px;
+    height: 60px;
+  }
+</style>
