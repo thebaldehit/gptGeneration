d1121125cb777952eccd34629b55fae8a3e2e19b
thebladehit <boqdanyarmolka@gmail.com>
2024-02-13 23:28:46 +0200

    Initital setup

---
+++ src/gptGeneration/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<div class="container">
+  <h1>Text</h1>
+</div>
